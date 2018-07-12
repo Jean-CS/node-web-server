c11c2542d393d845ba4fd99060db41b87825bf53
Jean Santos <jeansantosc@gmail.com>
2018-07-11 23:09:02 -0300

    adding handlebars

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+// tell express to use the Handlebars (hbs) view engine
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,6 +22,10 @@ app.get('/', (req, res) => {
     ]
   });
 });
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs');
+})
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
