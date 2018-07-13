eb5156bfb1383af903ee19f53ed55b1acf3f6865
Jean Santos <jeansantosc@gmail.com>
2018-07-12 21:17:46 -0300

    some middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,6 +9,26 @@ hbs.registerPartials(__dirname + '/views/partials');
 
 // tell express to use the Handlebars (hbs) view engine
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  // if next(); is not called, nothing else will be executed
+  next();
+});
+
+// // nothing else beyond this middleware will be executed,since 'next()' is not called
+// app.use((req, res, next) => {
+//  res.render('maintenance.hbs');
+//});
 
 app.use(express.static(__dirname + '/public'));
 
